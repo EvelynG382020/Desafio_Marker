606d02d7d4a4a950f6aa56d65fe22b3e782cc4cf
evelyn gomez <evelyngomezfrez@gmail.com>
2021-07-23 22:50:19 -0400

    add some method

--- app/models/category.rb
+++ app/models/category.rb
@@ -7,7 +7,7 @@ class Category < ApplicationRecord
 
   has_and_belongs_to_many :markers, dependent: :nullify
   #has_many :markers , dependent: :destroy
-  #has_many :subcategories, class_name: "Category", optional: true, foreign_key: "category_id"
+  
 
   def all_the_child_categories
     children_categories = []
@@ -33,44 +33,5 @@ class Category < ApplicationRecord
     end
   end
 
-  # def self.build_data
-  #   categories = Category.where(category_id: nil)
-  #   build(categories)
-  # end
-
-  # def self.build(all)
-  #   st = all.map{|t| info ={
-  #     "id" => t.id,
-  #     "name"=> t.name,
-  #     "subcategory_id" => t.subcategory_name,
-  #     "type_id" => t.one_type,
-  #     "makers"=>t.many_markers,
-  #     "public" => t.public
-    
-  #   }}
-  # end
-
-  # def self.type_name(id)
-  #   Type.find_by(id: id).name
-  # end
-
-  # def subcategory_name
-  #   all = Category.references(:subcategory).where(category_id: id)
-
-  #   all.map{|v| info={
-  #     "id" => v.id,
-  #     "name" => v.name,
-  #     "subcategory_id" =>v.subcategory_name,
-  #     "markers" => v.many_markers
-  #   }}
-  # end
-
-  # def many_markers
-  #   Marker.references(:category).where(category_id: id)
-  # end
-
-  # def one_type
-  #   Type.references(:category).where(id: type_id)
-  # end
-
+  
 end
